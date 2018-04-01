0f32c3c95364f2ee0219011e606d06471b035239
kwokh <kwokh@umich.edu>
2018-04-01 18:33:34 -0400

    initial files

---
+++ musicnotes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class MusicNote:
+    """
+    MusicNote is a library that fetches the specific musical pitch
+    given an audio file.
+    """
+
+    def __init__(self, filename):
+        """Initalize MusicNote class."""
+        
