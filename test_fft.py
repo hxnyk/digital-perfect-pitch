d9c63b098af89815ab1e43bdf6855734be8dce50
aanvi <aanvi@umich.edu>
2018-04-01 19:43:11 -0400

    change test

--- test_fft.py
+++ test_fft.py
@@ -4,24 +4,30 @@ from scipy.io import wavfile
 from scipy.signal import correlate
 import math 
 
-fs, data = wavfile.read('note_data/c.wav')
+fs, data = wavfile.read('note_data/a.wav')
 a = data.T[0] 
-b = fft(a)
+b=[(ele/2**8.)*2-1 for ele in a]
+c = fft(b)
+
 '''
 d = len(c)/2
 
 plt.plot(abs(c[:(d-1)]),'r') 
 plt.show()'''
 
-fs1, data1 = wavfile.read('note_data/d.wav')
+fs1, data1 = wavfile.read('note_data/c.wav')
 a1 = data1.T[0]
-b1 = fft(a1)
+print len(data1)
+print type(data1)
+b1=[(ele/2**8.)*2-1 for ele in a1]
+c1 = fft(b1)
 
 corr = 0
-for i in range(0, len(b)):
-    corr += abs(b[i] - b1[i])
+for i in range(0, len(c)):
+    corr += abs(c[i] - c1[i])
 print "Distance between C and D: ", corr
 
+'''
 fs2, data2 = wavfile.read('note_data/c.wav')
 a2 = data2.T[0]
 b2 = fft(a2)
@@ -31,7 +37,7 @@ for i in range(0, len(b)):
     corr += abs(b[i] - b2[i])
 print "Distance between C and C: ", corr 
   
-'''
+
 correlate = correlate(b, b1, 'full')
 for num in correlate: 
     print num
