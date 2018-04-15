6f765b711437e36d4e7b178188219f99a9e87a6a
aanvi <aanvi@umich.edu>
2018-04-15 14:53:14 -0400

    something

--- driver.py
+++ driver.py
@@ -10,13 +10,15 @@ def print_output(note):
     else: 
         octave_word = "octave"    
     print("Note is: ", note[0], "\nFrequency is: ", str(note[1]), "\nThis octave is", note[3], octave_word, note[2], "than training data")
+
 def main():
     tester = MusicNote()
-    """
+    '''
     for fi in os.listdir("note_data/"):
         note = tester.GetNote("note_data/" + fi)
         print(note)
-    
+    '''    
+
     print("Playing 'b' note")
     note = tester.GetNote("test_data/b1.wav")
     print_output(note)
@@ -24,24 +26,15 @@ def main():
     print("Playing 'g' note")
     note = tester.GetNote("test_data/g1.wav")
     print_output(note)
-
     print("Playing 'f' note")
     note = tester.GetNote("test_data/f1.wav")
     print_output(note)
-
+    
     print("Playing 'a' note")
     note = tester.GetNote("test_data/a1.wav")
     print_output(note)
     
-    print("Playing 'c' note")
-    note = tester.GetNote("test_data/c1.wav")
-    print_output(note)    
 
-    print("Playing 'd' note")
-    note = tester.GetNote("test_data/d1.wav")
-    print_output(note)   
-    """
-     
 
 if __name__ == "__main__":
     main()
